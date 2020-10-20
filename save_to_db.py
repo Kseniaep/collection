1c57cf8b41bee88b8519dfc39b79cce7ef1ce2aa
kseniaep <ksuxampei@yandex.ru>
2020-10-20 21:09:31 +0300

    Add load_new_data and print_salary_gte

--- save_to_db.py
+++ save_to_db.py
@@ -1,36 +1,78 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from datetime import datetime
 from pprint import pprint
 import pandas as pd
 from pymongo import MongoClient
 from glob import glob
 
-LOAD_PATH = './check'
+LOAD_PATH = './data'
 
 client = MongoClient('localhost', 27017)
 db = client.vacancies
 
-# load parsing data
-data_files = glob(LOAD_PATH + '/*.pkl')
 
-for file in data_files:
-    df = pd.read_pickle(file)
-    if len(df)>0:
-        db.vacancy.insert_many(df.to_dict('records'))
+def load_all_data (path):
+    """
+    Load parsed data from df in pickles to Mongodb
+    """
+    data_files = glob(LOAD_PATH + '/*.pkl')
+    print(data_files)
+    for file in data_files:
+        df = pd.read_pickle(file)
+        if len(df)>0:
+            db.vacancy.insert_many(df.to_dict('records'))
 
-""" Check salary 60 000"""
-find_res = db.vacancy.find({ "$and" : [ { "$or" : [ { "from" : { "$lte" : 60000 } }, { "from" : None } ] }, {"$or":[{"to":{"$gte" :60000}},{"to":None}]}]})
 
-for rec in find_res:
-    pprint(rec)
+def load_new_data(path):
+    """
+    Load parsed data from df in pickles to Mongodb
+    """
+    data_files = glob(path + '/*.pkl')
+    print(data_files)
+    for file in data_files:
+        counter = 0
+        df = pd.read_pickle(file)
+        if len(df)>0:
+            for row in df.to_dict('records'):
+                filter = {'name': row['name'],
+                          'link': row['link'],
+                          'from': row['from'],
+                          'to': row['to'],
+                          'currency': row['currency']}
 
-db.vacancy.count_documents({})
+                if db.vacancy.count_documents(filter=filter) == 0:
+                    db.vacancy.insert_one(row)
+                    counter += 1
+        print(f'Loaded from {file} {counter} new vacancies.')
 
-#db.vacancy.delete_many({})
+def print_salary_gte (salary, include_without_salary=False, currency='руб.'):
+    """
+    Find documents with salary greater or equal salary
+    """
+    if include_without_salary:
+        filter = { "$and": [ { "$or" : [ { "from" : { "$lte": salary } }, { "from": float('nan')}] },\
+                                                {"$or":[{"to": {"$gte": salary}},{"to": float('nan')}]}]}
+    else:
+        filter = {"$and": [{"$or": [{"from": {"$lte": salary}}]}, \
+                           {"$or": [{"to": {"$gte": salary}}]},
+                           {'currency': currency}]}
+    find_res = db.vacancy.find(filter,{'name': 1, 'from': 1, 'to': 1, '_id': 0})
 
-#df.info()
+    for rec in find_res:
+        pprint(f'{rec["name"]} {rec["from"]} {rec["to"]}')
+
+    print(f'Total vacancies with salary >= {salary}: {db.vacancy.count_documents(filter=filter)}')
+
+
+path = LOAD_PATH
+load_new_data(path)
+
+print_salary_gte(60000)
+
+
+db.vacancy.delete_many({})
+
 
 
 
