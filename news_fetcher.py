f9f00769fb2f588f3a2c9c26a301e675cca8a182
kseniaep <ksuxampei@yandex.ru>
2020-10-23 09:46:39 +0300

    Done

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#import pandas as pd
 from lxml import html
 import requests
-from datetime import datetime
 import logging
-from pprint import pprint
-import re
+from datetime import datetime
+from pymongo import MongoClient
 
-#HEADERS = {'User Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-#HEADERS = {'User Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
 HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-NEWS_SRC = ['https://news.mail.ru', 'https://lenta.ru/parts/news/', 'https://yandex.ru/news/']
 
 logging.basicConfig(filename="parser.log",
                     format='%(asctime)s %(levelname)-5s %(message)s',
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-response = requests.get(NEWS_SRC[1], headers=HEADERS)
+client = MongoClient('localhost', 27017)
+db = client.news
 
-print(response.status_code)
 
-def get_mail_news(response):
-    logging.info('Start parsing news.mail.ru')
+def get_request(url):
+    response = requests.get(url, headers=HEADERS)
+    if str(response.status_code)[0] == '2':
+        return response
+    else:
+        raise Exception (f'Code {response.status_code}')
+
+def get_mail_news():
+    NEWS_SRC = 'https://news.mail.ru'
+    logging.debug(f'Start parsing {NEWS_SRC}')
+    try:
+        response = get_request(NEWS_SRC)
+    except Exception:
+        raise ValueError("Can't get news.")
     root = html.fromstring(response.text)
     news = root.xpath( '//div[contains(@class,"cols__inner")]' )
     news_list=[]
@@ -35,11 +42,16 @@ def get_mail_news(response):
         news_dict['link'] = one_news.xpath('.//a[contains(@class,"newsitem__title")]/@href')[0]
         news_dict['title'] = one_news.xpath('.//a[contains(@class,"newsitem__title")]/span/text()')[0].replace('\xa0', ' ')
         news_list.append(news_dict)
-    logging.info(f'Got {len(news_list)} news')
+    logging.debug(f'Got {len(news_list)} news')
     return news_list
 
-def get_yandex_news(response):
-    logging.info('Start parsing news.yandex.ru')
+def get_yandex_news():
+    NEWS_SRC = 'https://yandex.ru/news/'
+    logging.debug(f'Start parsing {NEWS_SRC}')
+    try:
+        response = get_request(NEWS_SRC)
+    except Exception:
+        raise ValueError("Can't get news.")
     root = html.fromstring(response.text)
     news = root.xpath( '//article[contains(@class,"news-card")]' )
     news_list=[]
@@ -50,12 +62,17 @@ def get_yandex_news(response):
         news_dict['link'] = one_news.xpath('.//a[contains(@class,"news-card__link")]/@href')[0]
         news_dict['title'] = one_news.xpath('.//h2[contains(@class,"news-card__title")]/text()')[0].replace('\xa0', ' ')
         news_list.append(news_dict)
-    logging.info(f'Got {len(news_list)} news')
-    pprint(news_list)
+    logging.debug(f'Got {len(news_list)} news')
     return news_list
 
-def get_lenta_news(response):
-    logging.info('Start parsing lenta.ru')
+
+def get_lenta_news():
+    NEWS_SRC = 'https://lenta.ru/parts/news/'
+    logging.debug(f'Start parsing {NEWS_SRC}')
+    try:
+        response = get_request(NEWS_SRC)
+    except Exception:
+        raise ValueError("Can't get web page.")
     root = html.fromstring(response.text)
     news = root.xpath( '//div[contains(@class,"item news")]' )
     news_list=[]
@@ -63,13 +80,42 @@ def get_lenta_news(response):
         news_dict = {}
         news_dict['date'] = datetime.today().strftime('%Y-%m-%d') + ' ' + one_news.xpath('.//div[@class="info g-date item__info"]/text()')[0]
         news_dict['news_agency'] = 'lenta.ru'
-        news_dict['link'] = 'https://lenta.ru' + one_news.xpath('.//h3/a/@href')[0]
+        news_dict['link'] = one_news.xpath('.//h3/a/@href')[0]
         news_dict['title'] = one_news.xpath('.//h3/a/text()')[0].replace('\xa0', ' ')
         news_list.append(news_dict)
-    logging.info(f'Got {len(news_list)} news')
-    pprint(news_list)
+    logging.debug(f'Got {len(news_list)} news')
     return news_list
 
-#get_mail_news(response)
-#get_yandex_news(response)
-get_lenta_news(response)
+def load_new_data(records):
+    """
+    Load news to Mongodb
+    """
+    counter = 0
+    for record in records:
+        filter = {'date': record['date'],
+                  'news_agency': record['news_agency'],
+                  'link': record['link'],
+                  'title': record['title']
+                 }
+        if db.news.count_documents(filter=filter) == 0:
+            db.news.insert_one(record)
+            counter += 1
+    print(f'Loaded {counter} news.')
+
+def get_and_load_all():
+    mail_news = get_mail_news()
+    load_new_data(mail_news)
+
+    lenta_news = get_lenta_news()
+    load_new_data(lenta_news)
+
+    yandex_news = get_yandex_news()
+    load_new_data(yandex_news)
+
+get_and_load_all()
+
+for rec in db.news.find({},{'title': 1, '_id': 0}):
+    print(rec['title'])
+
+# db.news.delete_many({})
+
